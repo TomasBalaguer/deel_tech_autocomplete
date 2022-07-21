ef6a83933174d1948c03e4b0d405b86e0c0f874f
Tomas Balaguer <tomasbalaguer@192.168.1.5>
2022-07-21 17:52:20 -0300

    modified coutriesapi

--- src/services/countriesApi.ts
+++ src/services/countriesApi.ts
@@ -12,18 +12,7 @@ function getCountries(): Promise<Countries[]> {
 }
 
 
-function getCountry(value: string): Promise<Countries> {
-
-    return fetch(`https://restcountries.com/v3.1/alpha/${value}`)
-            .then(res => res.json())
-            .then(res => {
-                    return res as Countries
-            })
-}
-
-
 
 export {
-    getCountries,
-    getCountry
+    getCountries
 }
